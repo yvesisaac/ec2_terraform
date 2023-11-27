de35aff85463e6b134a430fa5336c176023d80cb
Isaac Yves <93200326+yvesisaac@users.noreply.github.com>
2023-11-27 14:14:21 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,27 @@
+variable "aws_region"{
+    type = string
+    description = ""
+    default = "us-east-1"
+}
 
+variable "instance_ami"{
+    type = string
+    description = ""
+    default = "ami-0230bd60aa48260c6"
+}
+
+variable "instance_type"{
+    type = string
+    description = ""
+    default = "t3.micro"
+}
+
+variable "instance_tags" {
+  type        = map(string)
+  description = ""
+
+  default = {
+    Name    = "linuxIsaac"
+    Project = "awsterraform"
+  }
+}
