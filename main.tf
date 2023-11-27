41aa6bd4305e9aefed1e06774bc0ec3ce6b455df
Isaac Yves <93200326+yvesisaac@users.noreply.github.com>
2023-11-27 14:14:58 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_version = "1.6.4"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+}
+
+
+resource "aws_instance" "web" {
+  ami           = var.instance_ami
+  instance_type = var.instance_type
+
+
+tags = var.instance_tags
+}
